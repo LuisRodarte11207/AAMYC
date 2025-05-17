c53754532c0f1fb9a288043bf10aed437f66f024
LuisRodarte11207 <luis.rodarte11207@gmail.com>
2025-05-17 09:54:39 -0600

    Updated files
    Updated index, script, style and animations

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ function productdisplay(page) {
   const one = document.getElementById("prodone");
   const two = document.getElementById("prodtwo");
   const three = document.getElementById("prodthree");
+  const four = document.getElementById("prodfour");
+  const five = document.getElementById("prodfive");
+  const six = document.getElementById("prodsix");
 
   switch (page) {
     case "one":
@@ -10,17 +13,53 @@ function productdisplay(page) {
       one.style = "display: initial;";
       two.style = "display: none;";
       three.style = "display: none;";
+      four.style = "display: none;";
+      five.style = "display: none;";
+      six.style = "display: none;";
       break;
     case "two":
       title.innerHTML = "Equipamiento médico";
-      two.style = "display: initial;";
       one.style = "display: none;";
+      two.style = "display: initial;";
       three.style = "display: none;";
+      four.style = "display: none;";
+      five.style = "display: none;";
+      six.style = "display: none;";
       break;
     case "three":
-      three.style = "display: initial;";
+      title.innerHTML = "Limpieza y esterilización";
       one.style = "display: none;";
       two.style = "display: none;";
+      three.style = "display: initial;";
+      four.style = "display: none;";
+      five.style = "display: none;";
+      six.style = "display: none;";
       break;
+    case "four":
+      title.innerHTML = "Construcción y mantenimiento";
+      one.style = "display: none;";
+      two.style = "display: none;";
+      three.style = "display: none;";
+      four.style = "display: initial;";
+      five.style = "display: none;";
+      six.style = "display: none;";
+      break;
+    case "five":
+      title.innerHTML = "Servicios médicos especializados";
+      one.style = "display: none;";
+      two.style = "display: none;";
+      three.style = "display: none;";
+      four.style = "display: none;";
+      five.style = "display: initial;";
+      six.style = "display: none;";
+      break;
+    case "six":
+      title.innerHTML = "Otros";
+      one.style = "display: none;";
+      two.style = "display: none;";
+      three.style = "display: none;";
+      four.style = "display: none;";
+      five.style = "display: none;";
+      six.style = "display: initial;";
   }
 }
